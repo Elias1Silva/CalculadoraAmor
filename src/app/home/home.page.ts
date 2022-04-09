1f83906141221441a489d1734518d84a1bcf8715
Elias1Silva <elias.faria01@etec.sp.gov.br>
2022-04-08 22:41:21 -0300

    projeto finalizado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  load = false
+  nome1 = ""
+  nome2 = ""
+  url = "http://lucasreno.kinghost.net/love-calculator/"
+  resultado = 0
+  relacao = ""
+  constructor(public http: HttpClient,) {}
+  enviarDados(){
+    this.http.get<any>(this.url+this.nome1+"/"+ this.nome2).subscribe(
+      async (resposta: any) =>{
+        this.relacao="";
+        this.load = true;
+        for(let i=0; i<=resposta; i++){
+          this.resultado = i;
+          await new Promise(f => setTimeout(f, 50));
+        }
+        this.load = false;
 
-  constructor() {}
-
+        if(this.resultado < 20) this.relacao = "Esquece e vai pra outra(o)";
+        else if(this.resultado < 40) this.relacao = "Vocês não tem os mesmos gostos";
+        else if(this.resultado < 60) this.relacao = "Se deu deu se não deu Fu#*@";
+        else if(this.resultado < 80) this.relacao = "Vocês são um casal quase perfeito";
+        else if(this.resultado == 100) this.relacao ="Parabéns você achou sua alma gêmea";
+         else this.relacao = "Lança seu charme";
+      }
+    );
+  }
 }
